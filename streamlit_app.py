027fba027caa80343f65abd1e701812497a262af
pranut7peaks <pranut.pannao@7peakssoftware.com>
2026-02-19 17:02:23 +0700

    Update fruit options selection in streamlit_app.py
    Changed selection from 'FRUIT_NAME' to 'FRUIT_ID' in the Snowpark Dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_ID'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
